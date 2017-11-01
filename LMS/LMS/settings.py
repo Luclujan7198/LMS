f5b8eb72bd82ce3144af6293ae6eeda4c17b4adf
Guilherme Galassi Scarpitta <g.scarpitta@reply.com>
2017-11-01 13:04:20 -0200

    site with navbar

--- LMS/LMS/settings.py
+++ LMS/LMS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'LMSWeb.apps.LmswebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
