4a0bc5be490684541caa83a70406f15b2a6570d1
Guilherme <g.scarpitta@reply.com>
2017-11-07 16:35:06 -0200

    Noticia e contato

--- LMS/LMSWeb/urls.py
+++ LMS/LMSWeb/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^cursos/$', views.cursos, name='cursos'),
     url(r'^cursos/(?P<curso_id>[0-9]+)/$', views.curso, name='curso'),
     url(r'^noticias/$', views.noticias, name='noticias'),
-    url(r'^noticias/(?P<noticia_id>[0-9]+)/$', views.noticia, name='noticia')
+    url(r'^noticias/(?P<noticia_id>[0-9]+)/$', views.noticia, name='noticia'),
+    url(r'^contato/$', views.contato, name="contato")
 ]
--- LMS/LMSWeb/views.py
+++ LMS/LMSWeb/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-from .models import Curso, Noticia
-
 def index(request):
     return render(request, 'LMSWeb/index.html')
 
@@ -9,14 +7,13 @@ def cursos(request):
     return render(request, 'LMSWeb/cursos.html')
 
 def curso(request, curso_id):
-    model = Curso()
-    curso = model.GetCurso(curso_id)
-    return render(request, 'LMSWeb/curso.html', { 'curso': curso })
+    return render(request, 'LMSWeb/curso.html', { 'curso_id': curso_id })
 
 def noticias(request):
     return render(request, 'LMSWeb/noticias.html')
 
 def noticia(request, noticia_id):
-    model = Noticia()
-    noticia = model.GetNoticia(noticia_id)
-    return render(request, 'LMSWeb/noticia.html', { 'noticia': noticia })
+    return render(request, 'LMSWeb/noticia.html', { 'noticia_id': noticia_id })
+
+def contato(request):
+    return render(request, 'LMSWeb/contato.html')
