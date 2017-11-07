d06301b3ea1b3b4c4dda228990e0e16b2585ae9a
1701419 <1701419@fit.net>
2017-11-07 21:57:33 -0200

    cursos

--- LMS/core/models.py
+++ LMS/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Curso(models.Model):
+    sigla = models.CharField(max_length=10, default='')
     nome = models.CharField(max_length=200)
     matutino = models.BooleanField(default=False)
     vespertino = models.BooleanField(default=False)
@@ -10,3 +11,6 @@ class Curso(models.Model):
     integral = models.BooleanField(default=False)
     modalidade = models.CharField(max_length=200)
     duracao = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
