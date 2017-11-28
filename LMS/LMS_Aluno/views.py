403fb2151ba04b024fa651c3d585c9dce0261070
luclujan7198 <lujanhfu@gmail.com>
2017-11-28 19:35:26 -0200

    verificação perfil

--- LMS/LMS_Aluno/views.py
+++ LMS/LMS_Aluno/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
+
+def usuario_is_aluno(user):
+    return user.groups.filter(name='Alunos').exists()
+
+
+@user_passes_test(usuario_is_aluno, login_url='/login')
 def index(request):
     return render(request, 'LMS_Aluno/index.html')
 
+@user_passes_test(usuario_is_aluno, login_url='/login')
 def boletim(request):
     return render(request, 'LMS_Aluno/boletim.html')
--- LMS/LMS_Professor/views.py
+++ LMS/LMS_Professor/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
+def usuario_is_professor(user):
+    return user.groups.filter(name='Professores').exists()
+
+
+@user_passes_test(usuario_is_professor, login_url='/login')
 def teste(request):
     return render(request, 'LMS_Professor/testes.html')
 
+@user_passes_test(usuario_is_professor)
 def index(request):
     return render(request, 'LMS_Professor/index.html')
